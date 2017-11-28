a6acb2359c35d1f0e5ee31f88833a4791bf2ecf0
f1nal1ty <saviom.cedo@hotmail.com>
2017-11-28 13:57:29 -0200

    Estrutura
    Estrutura MVC

---
+++ Projeto/Projeto/Controllers/ExemploController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Projeto.Controllers
+{
+    class ExemploController
+    {
+        public ExemploController() { }
+
+        public void HelloWorld()
+        {
+            Console.WriteLine("Esta menssagem veio do controller.");
+        }
+    }
+}
---
+++ Projeto/Projeto/Program.cs
@@ -0,0 +1,15 @@
+﻿using Projeto.Controllers;
+using System;
+
+namespace Projeto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ExemploController exemplo = new ExemploController();
+            exemplo.HelloWorld();
+            Console.ReadKey();
+        }
+    }
+}
